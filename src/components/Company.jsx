89db98e2ea8cbc07dc2d95999edb890652969428
Gary Alway <gary.alway@acuris.com>
2020-10-05 19:36:56 +0100

    company content update

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -7,38 +7,42 @@ const P = styled.p`
 `
 
 export const Company = () => (
-  <Box title="Z.P.H. KORA - Jedyny partner firmy JC Auto na Podhale">
+  <Box title="Samochody to nasza pasja">
     <P>
-      Nasz sklep motoryzacyjny jest partnerem handlowych JC AUTO oraz HIPOL ,oferuje szeroką gamę
-      częśści zamiennych do samochodów japońskich, koreańskich jak i europejskich, zarówno
-      eksploatacyjnych, jak i silnikowych.
+      Sklep motoryzacyjny Kora to rodzinna firma z długoletnią tradycją. Naszym celem jest
+      zapewnienie klientom doskonałej obsługi, a dzięki ponad 40-letniemu doświadczeniu czujemy, że
+      jesteśmy w stanie to zapewnić. Właściciel sklepu, mgr. inż Zbigniew Ptaś, dba o bezpośredni
+      kontakt z odbiorcą, gwarantuje fachową obsługę i służy rzetelną informacją.
     </P>
     <P>
-      Posiadamy w stałej sprzedaży części eksploatacyjne /filtry, klocki ,tarcze hamulcowe, szczęki,
-      końcówki drążków, pompy wody, paliwa, tarcze hamulcowe, tuleje metalowo-gumowe zawieszenia
-      ,łączniki stabilizatora, amortyzatory ,elementy blacharskie, oświetlenie ,zderzaki, jak
-      również oleje , smary i plyny chłodnicze, oraz wiele wiele innych.
+      Oferujemy szeroką gamę części zamiennych do samochodów eksploatacyjnych, jak i silnikowych.
+      Serdecznie zapraszamy do współpracy zarówno warsztaty, stacje obsługi, jak i klientów
+      detalicznych.
     </P>
     <P>
-      W ofercie posiadamy całą gamę oferty JC AUTO, oraz HIPOL. W przypadku braku części w naszym
-      magazynie otrzymujemy dwie dostawy części dziennie ;1-sza do południa 11:00 druga 16:00,
-      części zamówione do godz.13:00 są do odebrania o godz.16:00 .Zamówione później będą do
-      odebrania w następnym dniu o godz.11:00.
+      Warto zaglądnąć na naszą zakładkę Sklep Online (Allegro), gdzie oferujemy najciekawsze
+      artykuły w najniższych cenach, możliwe do odebrania osobiście lub wysłania w dowolne miejsce
+      Polski.
     </P>
     <P>
-      Dostarczamy nasz towar dla stałych odbiorców na terenie Nowego Targu i na trasie nowy Targ
-      Szczawnica.
-    </P>
-    <P>Nasi pracownicy zapewnią komfortową obsługę i fachowo dobiorą odpowiedni towar.</P>
-    <P>
-      Warto zaglądnąć na naszą zakładkę Allegro, gdzie oferujemy najciekawsze artykuły w najniższych
-      cenach, możliwe do odebrania osobiście lub wysłania w dowolne miejsce Polski.
-    </P>
-    <P>
-      W zakładce produkt można oglądnąć listę firm, których towarem handlujemy, ale chcemy
-      zaznaczyć, że lista dostawców ciągle się rozszerza, gdyż robimy wszystko aby sprostać
-      oczekiwaniom naszych klientów i regularnie poszerzamy asortyment o najnowsze marki i modele
-      samochodów.
+      Posiadamy w stałej sprzedaży:
+      <ul>
+        <li>części eksploatacyjne /filtry</li>
+        <li>klocki</li>
+        <li>tarcze hamulcowe</li>
+        <li>szczęki</li>
+        <li>końcówki drążków</li>
+        <li>pompy wody</li>
+        <li>paliwa</li>
+        <li>tarcze hamulcowe</li>
+        <li>tuleje metalowo-gumowe zawieszenia</li>
+        <li>łączniki stabilizatora</li>
+        <li>amortyzatory</li>
+        <li>elementy blacharskie</li>
+        <li>oświetlenie </li>
+        <li>zderzaki</li>
+        <li>oleje, smary i plyny chłodnicze, oraz wiele wiele innych produktów</li>
+      </ul>
     </P>
   </Box>
 )
